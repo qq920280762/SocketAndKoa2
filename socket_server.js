0629fcd09b46dcd5a2e642881e7184fc8142eb67
gongchao <920280762@qq.com>
2017-04-14 16:41:21 +0800

    formatting code

--- socket_server.js
+++ socket_server.js
@@ -1,98 +1,60 @@
 'use strict';
 
-/**
-
-// 加入manager分组
-// socket.on('group1', function () {
-//            socket.join('group1');
-//            //包括自己
-//            io.sockets.in('group1').emit('msgReceived',  ' Welcome '+socket.id+' to join group1');
-//            //不包括自己
-//            socket.broadcast.to('group1').emit('msgReceived', ' Welcome '+socket.id+' to join group1');
-//            //获取所有房间（分组）信息,返回{socket_id:true,...}
-//            io.sockets.adapter.rooms
-//            //获取group1中的客户端，返回{socket_id:true,...}
-//            io.sockets.adapter.rooms('group1');
-//            //移除房间
-//            socket.leave('group1');
-//   });
-
-// exports.start = function(server){
-//
-//    var io  = require('socket.io')(server);
-//
-//    io.on('connection', function(socket) {
-//
-//        socket.name = randomNames.getRandomName(2);
-//
-//        console.log('soket connection [ '+socket.id+' ] ');
-//
-//        //加入employee分组
-//        socket.on('roomJoin',function(data){
-//            socket.join(data.id);
-//            socket.emit('msgReceived', ' [ '+socket.name+' ] => JOIN ROOM '+data.id);
-//            socket.broadcast.to(data.id).emit('msgReceived', ' [ '+socket.name+' ] => JOIN ROOM '+data.id);
-//        });
-//
-//        socket.on('roomLeave',function(data){
-//            socket.leave(data.id);
-//            socket.emit('msgReceived', ' [ '+socket.name+' ] => LEAVE ROOM '+data.id);
-//            socket.broadcast.to(data.id).emit('msgReceived', ' [ '+socket.name+' ] => LEAVE ROOM '+data.id);
-//        });
-//
-//        socket.on('roomChat',function(data){
-//            socket.emit('msgReceived',' [ '+socket.name+' ] => '+ data.msg);
-//            socket.broadcast.to(data.id).emit('msgReceived',' [ '+socket.name+' ] => '+ data.msg);
-//        });
-//
-//        //客户端断开事件
-//        socket.on('disconnect', function () {
-//            console.log('client active disconnect  [  '+socket.id+' ]');
-//            socket.disconnect();
-//        });
-//
-//    });
-// }
-
- */
-
-
-let randomNames = require('./utils/randomNames');
-let socketUtils = require('./utils/SocketBase');
+const randomNames = require('./utils/randomNames');
+const socketUtils = require('./utils/SocketBase');
 
 class socket_server extends socketUtils {
 
-    constructor(server){
+    constructor(server) {
+
         super(require('socket.io')(server));
     }
 
-    init(){
-        this.connect( (socket) => {
+    init() {
+
+        this.connect((socket) => {
+
             socket.name = randomNames.getRandomName(2);
-            console.log('soket connection [ '+socket.id+' ] ');
-            // 加入employee分组
-            this.on('roomJoin',socket.id,(data)=>{
-                if(!this.hasSocket(data.id,socket.id)){
-                    this.join(data.id,socket.id);
-                    this.broadcast('msgReceived',' [ '+socket.name+' ] => JOIN ROOM '+data.id ,data.id);
-                }else{
-                    this.emit('msgReceived',' [ '+socket.name+' ] => Repeat to join the room '+data.id,socket.id);
+
+            console.log('soket connection [ ' + socket.id + ' ] ');
+
+            this.on('roomJoin', socket.id, (data)=> {
+
+                if (!this.hasSocket(data.id, socket.id)) {
+
+                    this.join(data.id, socket.id);
+
+                    this.broadcast('msgReceived', ' [ ' + socket.name + ' ] => JOIN ROOM ' + data.id, data.id);
                 }
+                else {
+
+                    this.emit('msgReceived', ' [ ' + socket.name + ' ] => Repeat to join the room ' + data.id, socket.id);
+
+                }
+
             });
 
-            this.on('roomLeave',socket.id,(data)=>{
-                this.leave(data.id,socket.id);
-                this.broadcast('msgReceived', ' [ '+socket.name+' ] => LEAVE ROOM '+data.id,data.id);
+            this.on('roomLeave', socket.id, (data)=> {
+
+                this.leave(data.id, socket.id);
+
+                this.broadcast('msgReceived', ' [ ' + socket.name + ' ] => LEAVE ROOM ' + data.id, data.id);
+
             });
 
-            this.on('roomChat',socket.id,(data)=>{
-                this.broadcast('msgReceived',' [ '+socket.name+' ] => '+ data.msg,data.id);
+            this.on('roomChat', socket.id, (data)=> {
+
+                this.broadcast('msgReceived', ' [ ' + socket.name + ' ] => ' + data.msg, data.id);
+
             });
 
             //客户端断开事件
-            this.on('disconnect',socket.id,() => {
-                console.log('client active disconnect  [  '+socket.id+' ]');
+            this.on('disconnect', socket.id, () => {
+
+                console.log('client active disconnect  [  ' + socket.id + ' ]');
+
                 this.disconnect(socket.id);
+
             });
 
         });
--- utils/SocketBase.js
+++ utils/SocketBase.js
@@ -1,75 +1,123 @@
 'use strict';
+
+
 class SocketBase {
 
-    constructor(io){
+    constructor(io) {
+
         this.io = io;
+
     };
 
 
-    connect(fn){
-        this.io.on('connection',fn);
+    connect(fn) {
+
+        this.io.on('connection', fn);
+
     };
 
-    on(event,socketId,fn){
+    on(event, socketId, fn) {
+
         let socket = this.io.sockets.sockets[socketId];
-        if(event && socket){
-            socket.on(event,fn);
+
+        if (event && socket) {
+
+            socket.on(event, fn);
+
         }
+
     };
 
-    join(roomId,socketId){
+    join(roomId, socketId) {
+
         let socket = this.io.sockets.sockets[socketId];
-        if(roomId && socket){
-           socket.join(roomId);
+
+        if (roomId && socket) {
+
+            socket.join(roomId);
+
         }
+
     };
 
-    leave(roomId,socketId){
+    leave(roomId, socketId) {
+
         let socket = this.io.sockets.sockets[socketId];
-        if(roomId && socket){
+
+        if (roomId && socket) {
+
             socket.leave(roomId);
+
         }
+
     };
 
-    broadcast(event,data,roomId){
-        if(roomId){
-            this.io.to(roomId).emit(event,data);
-        }else{
-            this.io.sockets.emit(event,data);
+    broadcast(event, data, roomId) {
+
+        if (roomId) {
+
+            this.io.to(roomId).emit(event, data);
+
         }
+        else {
+
+            this.io.sockets.emit(event, data);
+
+        }
+
     };
 
-    emit(event,data,socketId){
+    emit(event, data, socketId) {
+
         let socket = this.io.sockets.sockets[socketId];
-        if(socket){
-            socket.emit(event,data);
+
+        if (socket) {
+
+            socket.emit(event, data);
+
         }
 
     };
 
-    getSockets(room){
-        return room?this.io.sockets.adapter.rooms[room]:this.io.sockets.adapter.rooms;
+    getSockets(room) {
+
+        return room ? this.io.sockets.adapter.rooms[room] : this.io.sockets.adapter.rooms;
+
     };
 
-    hasSocket(room,socketId){
-        let rooms = (this.getSockets(room)||{}).sockets||{};
-        let flag = false;
-        for( let key in rooms){
-            if(socketId==key && !!rooms[key]){
+    hasSocket(room, socketId) {
+
+        let rooms = (this.getSockets(room) || {}).sockets || {};
+
+        let flag  = false;
+
+        for (let key in rooms) {
+
+            if (socketId == key && !!rooms[key]) {
+
                 flag = true;
+
                 break;
+
             }
+
         }
+
         return flag;
+
     };
 
-    disconnect(socketId){
+    disconnect(socketId) {
+
         let socket = this.io.sockets.sockets[socketId];
-        if(socket){
-            socket.disconnect();
-        }
-    };
 
+        if (socket) {
+
+            socket.disconnect();
+
+        }
+
+    };
 
 }
 module.exports = SocketBase;
