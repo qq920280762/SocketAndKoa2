b335552d73c4f71edc532a16e8ceaf2d73ff585d
gongchao <920280762@qq.com>
2017-04-17 10:13:22 +0800

    add route get_server_address

--- public/js/index.js
+++ public/js/index.js
@@ -1,68 +1,72 @@
-var socket = new io.connect('http://localhost:3000',{'reconnect':false,'auto connect':false});
 
-/* 自定义获取消息(msgReceived)事件 */
-socket.on('msgReceived', function (msg) {
-    $('#receive').append(msg+'\r\n');
-});
+$.get('/server/address',function(result){
 
-/* 连接事件 */
-socket.on('connect', function () {
-    $('#receive').append('[ '+socket.id+' ] connect to the server ~ \r\n');
-});
+    var socket = new io.connect(result,{'reconnect':false,'auto connect':false});
 
-/* 连接错误事件 */
-socket.on('connect_error',function(object){
-    $('#receive').append(' connect_error :'+JSON.stringify(object)+'\r\n');
-});
+    /* 自定义获取消息(msgReceived)事件 */
+    socket.on('msgReceived', function (msg) {
+        $('#receive').append(msg+'\r\n');
+    });
 
-/* 服务器断开事件 */
-socket.on('disconnect',function(){
-    $('#receive').append('server active disconnect ~ \r\n');
-    socket.disconnect();
-})
+    /* 连接事件 */
+    socket.on('connect', function () {
+        $('#receive').append('[ '+socket.id+' ] connect to the server ~ \r\n');
+    });
 
-/* 重新连接事件 大于三次就主动断开 */
-socket.on('reconnecting',function(number){
-    if(number>3){
+    /* 连接错误事件 */
+    socket.on('connect_error',function(object){
+        $('#receive').append(' connect_error :'+JSON.stringify(object)+'\r\n');
+    });
+
+    /* 服务器断开事件 */
+    socket.on('disconnect',function(){
+        $('#receive').append('server active disconnect ~ \r\n');
         socket.disconnect();
-        $('#receive').append('reconnecting number gt 3 ~ \r\n');
-    }
-});
+    })
 
-$('#open').click(function(){
-    socket.connect();
-});
-$('#close').click(function(){
-    socket.disconnect();
-});
-$('#send').click(function(){
-    if(!$('#msg').val().toString().trim()){
-        alert('Send content cannot be empty ~');
-        return;
+    /* 重新连接事件 大于三次就主动断开 */
+    socket.on('reconnecting',function(number){
+        if(number>3){
+            socket.disconnect();
+            $('#receive').append('reconnecting number gt 3 ~ \r\n');
+        }
+    });
+
+    $('#open').click(function(){
+        socket.connect();
+    });
+    $('#close').click(function(){
+        socket.disconnect();
+    });
+    $('#send').click(function(){
+        if(!$('#msg').val().toString().trim()){
+            alert('Send content cannot be empty ~');
+            return;
+        };
+        var data = {roomId:$('#room').val(),msg:$('#msg').val()};
+        socket.emit('roomChat',data);
+        $('#msg').val('');
+    });
+
+    $('#join-room').click(function(){
+        var data = {roomId:$('#room').val(),msg:$('#msg').val()};
+        socket.emit('roomJoin',data);
+    });
+
+    $('#leave-room').click(function(){
+        var data = {roomId:$('#room').val(),msg:$('#msg').val()};
+        socket.emit('roomLeave',data);
+    });
+
+    $('#clear').click(function(){
+        $('#receive').empty();
+    });
+
+    document.onkeydown=function(event){
+        var e = event || window.event || arguments.callee.caller.arguments[0];
+        if(e && e.keyCode==13){
+
+            $('#send').click();
+        }
     };
-    var data = {roomId:$('#room').val(),msg:$('#msg').val()};
-    socket.emit('roomChat',data);
-    $('#msg').val('');
 });
-
-$('#join-room').click(function(){
-    var data = {roomId:$('#room').val(),msg:$('#msg').val()};
-    socket.emit('roomJoin',data);
-});
-
-$('#leave-room').click(function(){
-    var data = {roomId:$('#room').val(),msg:$('#msg').val()};
-    socket.emit('roomLeave',data);
-});
-
-$('#clear').click(function(){
-    $('#receive').empty();
-});
-
-document.onkeydown=function(event){
-    var e = event || window.event || arguments.callee.caller.arguments[0];
-    if(e && e.keyCode==13){
-
-        $('#send').click();
-    }
-};
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 'use strict';
 const router = require('koa-router')();
 const user = require('./user');
+const config = require('../config');
 
 router.use('/user',user.routes());
 
-let get_index = async (ctx,next)=>{
+let get_index = async (ctx)=>{
     ctx.session.user = 'The winner';
     ctx.state.title = 'welcome to koa2 ~';
 
     await ctx.render('index');
 };
 
+let get_server_address = async (ctx)=>{
+    ctx.body = 'http://'+config.server.hostname+':'+config.server.port;
+};
+
+router.get('/server/address',get_server_address);
 
 router.get('/',get_index);
 module.exports = router;
